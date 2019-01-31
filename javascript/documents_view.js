c9837a6d23fd7b829b2802a6a24998a1027b9a66
Mr P Upfold <upfoldp@testvalley.hants.sch.uk>
2019-01-31 14:18:54 +0000

    Pulls document in via JSON

--- javascript/documents_view.js
+++ javascript/documents_view.js
@@ -1,4 +1,18 @@
-this.documentItemClick = function() {
+
+this.documentItemClick = function(id) {
+
+	var token = this.CoreSitesProvider.currentSite.getToken();
+
+	let wsPreSets = {
+            wsToken: token,
+            siteUrl: this.CoreSitesProvider.getCurrentSite().siteUrl,
+            cleanUnicode: false,
+            typeExpected: 'object',
+            responseExpected: true
+	};
+
 	debugger;
-	alert('test');
+	this.CoreWSProvider.call('report_parentprogressview_get_document', { 'id' : id }, wsPreSets).then(function(result) {
+		debugger;
+	});
 }
