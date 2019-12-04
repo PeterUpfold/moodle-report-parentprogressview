15ea94dfd1c5bee5b7241e757f8fa2f9ec745123
Mr P Upfold <upfoldp@testvalley.hants.sch.uk>
2019-12-04 12:33:39 +0000

    Bump version

--- version.php
+++ version.php
@@ -27,7 +27,7 @@ Parent Progress View, a module for Moodle to allow the viewing of documents and 
  
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2019072601;			// YYYYMMDDXX where XX is an incrementing revision number for that day
+$plugin->version = 2019120400;			// YYYYMMDDXX where XX is an incrementing revision number for that day
 $plugin->requires  = 2018051708;			// required Moodle version.tring
 $plugin->component = 'report_parentprogressview';	// Full name of the plugin
 $plugin->maturity  = MATURITY_STABLE;			// why not?
