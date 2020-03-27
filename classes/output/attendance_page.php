81e67d21d60074299d400130ece885e96cba5926
Mr P Upfold <upfoldp@testvalley.hants.sch.uk>
2020-03-27 14:45:52 +0000

    Only render mobile views if backend responded 200

--- classes/output/attendance_page.php
+++ classes/output/attendance_page.php
@@ -212,11 +212,13 @@ class attendance_page implements renderable, templatable {
 						'mark_description'
 					)
 				);
-				try {
-					$output[$o_count]->ion_grid = $ion_grid->render();
-				}
-				catch (\InvalidArgumentException $iae) {
-					error_log('The data passed to the ion-grid renderer was not an array of stdClass objects, despite having >0 count -- data for ' . $username);
+				if ($request->status == 200) {
+					try {
+						$output[$o_count]->ion_grid = $ion_grid->render();
+					}
+					catch (\InvalidArgumentException $iae) {
+						error_log('The data passed to the ion-grid renderer was not an array of stdClass objects, despite having >0 count -- data for ' . $username);
+					}
 				}
 
 				// fill summary data with placeholder strings for now -- replace below if we get a data match
